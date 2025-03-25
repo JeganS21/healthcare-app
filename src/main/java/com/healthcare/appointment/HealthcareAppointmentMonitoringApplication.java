7aa35d805480b3e789d9b5fb191de0daa383feb8
JeganS21 <jegan994322@gmail.com>
2025-03-25 21:49:19 +0530

    Created Setup for Health Care Appointment Monitoring Application using Java Springboot

---
+++ src/main/java/com/healthcare/appointment/HealthcareAppointmentMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareAppointmentMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareAppointmentMonitoringApplication.class, args);
+	}
+
+}
